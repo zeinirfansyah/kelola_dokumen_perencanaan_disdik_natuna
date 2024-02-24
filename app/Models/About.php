a4c4c9373c438477ed715d3db1019acc0d8d1c30
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-24 21:35:32 +0700

    add: About model

--- app/Models/About.php
+++ app/Models/About.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class About extends Model
 {
     use HasFactory;
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'tentang_disdik',
+        'alamat',
+        'no_telepon',
+        'instagram',
+        'email',
+        'kepala_dinas',
+        'quotes'
+    ];
+
+    /**
+     * A description of the entire PHP function.
+     *
+     * @param datatype $paramname description
+     * @throws Some_Exception_Class description of exception
+     * @return Some_Return_Value
+     */
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
