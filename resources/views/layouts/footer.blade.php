b45dce1f4821a5aa6c22d09958defc777fd88543
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-25 20:12:07 +0700

    add: footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,14 @@
-<footer class="main-footer text-center py-3">
-  <strong>
-    Dinas Pendidikan Natuna<a href="https://zeinirfansyah.me" target="_blank" style="text-decoration: none; color: #000">.</a></strong>    
+<footer class="main-footer py-3 ">
+  <div class="container d-flex">
+    <div class="cr-dinas text-start">
+      <strong class="text-start">
+        Dinas Pendidikan Natuna<a href="https://zeinirfansyah.me" target="_blank"
+          style="text-decoration: none;">.</a>
+        </strong>
+    </div>
+    <div class="cr-icons8 ms-auto me-0">
+      <a target="_blank" href="https://icons8.com/icon/Tm4KFLoi1Z0V/document">Document</a> icon by <a target="_blank"
+      href="https://icons8.com">Icons8</a>
+    </div>
+  </div>
 </footer>
