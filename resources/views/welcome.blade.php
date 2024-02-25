d28efc6eda7f338530a026e7c9e02a54c28f63d8
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-25 20:14:00 +0700

    fix: homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,24 @@
-@extends('visitor.home')
+@extends('layouts.app')
+
+@section('content')
+  <div class="home">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 col-md-6 dark-canvas">
+          <div class="home-text">
+            <h2><strong>Selamat Datang di Website Perencanaan Dinas Pendidikan Kabupaten Natuna</strong></h2>
+            @if ($about)
+              <p class="tnetang-kami">
+                {{ $about->tentang_disdik }}
+              </p>
+            @else
+              <p>No information available</p>
+            @endif
+          </div>
+        </div>
+        <div class="col">
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
