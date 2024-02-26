6c1c24ed243b25c869ef2055862361d6650c4dcd
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-26 11:47:43 +0700

    fix: remove alret demo

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -167,7 +167,7 @@
         <!-- AdminLTE App -->
         <script src="{{ asset('/AdminLTE/dist/js/adminlte.js') }}"></script>
         <!-- AdminLTE for demo purposes -->
-        <script src="{{ asset('/AdminLTE/dist/js/demo.js') }}"></script>
+        <!-- <script src="{{ asset('/AdminLTE/dist/js/demo.js') }}"></script> -->
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="{{
                 asset('/AdminLTE/dist/js/pages/dashboard.js')
