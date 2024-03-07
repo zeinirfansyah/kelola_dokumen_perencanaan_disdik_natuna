a74cf439be7d58f119ddff87123eba4655583421
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-03-07 15:25:11 +0700

    fix [layout]: fix navbar layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,10 +58,8 @@
   </script>
 
   <!-- bootstrap 5.0 -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-dsi1h0ucU5Pkppe4lJS6qY8cpgJe7eSxF+qvazOlr06YmK6XcDqe5kMvVO92KqKJ" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,55 +1,74 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <strong>SIMBEK</strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+  <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">
+      <strong>SIMBEK</strong>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto">
-                <!-- add navigation -->
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.lkjip') }}">LKJIP</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.spmp') }}">SPM Pendidikan</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.pkrkt') }}">PK & RKT</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.peraturan') }}">Peraturan Pendidikan</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.sapras') }}">Sarana & Prasarana</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.kdp') }}">KDP</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.musrenbangkab') }}">Musrenbangkab</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.galery') }}">Galeri</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('landing.contact') }}">Kontak</a></li>
-            </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav me-auto">
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
-                @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
+        <li class="nav-item"><a class="nav-link" href="{{ route('landing.lkjip') }}">LKJIP</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('landing.spmp') }}">SPM Pendidikan</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('landing.pkrkt') }}">PK & RKT</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('landing.peraturan') }}">Peraturan Pendidikan</a></li>
+        <!-- Dropdown menu for the remaining items -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dokumen Lainnya
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item">
-                        <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
-                        Log out
-                      </a>
-            
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                      </form>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+            <a class="dropdown-item" href="{{ route('landing.sapras') }}">Sarana & Prasarana</a>
+            <a class="dropdown-item" href="{{ route('landing.kdp') }}">KDP</a>
+            <a class="dropdown-item" href="{{ route('landing.musrenbangkab') }}">Musrenbangkab</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Galeri & Kontak
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+            <a class="dropdown-item" href="{{ route('landing.galery') }}">Galeri</a>
+            <a class="dropdown-item" href="{{ route('landing.contact') }}">Kontak</a>
+          </div>
+        </li>
+      </ul>
+
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ms-auto">
+        <!-- Authentication Links -->
+        @guest
+          @if (Route::has('login'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+          @endif
+
+          @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+          @endif
+        @else
+          <li class="nav-item">
+            <a href="{{ route('logout') }}"
+              onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
+              Log out
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+          </li>
+        @endguest
+      </ul>
     </div>
+  </div>
 </nav>
