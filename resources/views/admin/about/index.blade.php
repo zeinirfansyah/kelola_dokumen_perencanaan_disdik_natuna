6cec55f516d1377729387e42aa45826c1889d7dc
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-03-22 03:03:49 +0700

    fix: handle empty data

--- resources/views/admin/about/index.blade.php
+++ resources/views/admin/about/index.blade.php
@@ -35,59 +35,63 @@
             </div>
           </div>
           <div class="card-body">
-            <table class="table table-bordered mb-2">
-              <tr>
-                <td colspan="2">
-                  <div class="row">
-                    <div class="col-12 col-md-3">
-                      <div class="d-flex justify-content-center">
-                        <div class="image">
-                          <div class="row">
-                            <div class="col">
-                              <img src="{{ asset('storage/kepala_dinas/' . $about->foto_kepala_dinas) }}"
-                                class="img-fluid rounded"
-                                style="height: 300px; width: 200px; object-fit: cover;  border: 5px solid #d7d7d7;"
-                                alt="{{ $about->foto_kepala_dinas }}">
+            @if ($about)
+              <table class="table table-bordered mb-2">
+                <tr>
+                  <td colspan="2">
+                    <div class="row">
+                      <div class="col-12 col-md-3">
+                        <div class="d-flex justify-content-center">
+                          <div class="image">
+                            <div class="row">
+                              <div class="col">
+                                <img src="{{ asset('storage/kepala_dinas/' . $about->foto_kepala_dinas) }}"
+                                  class="img-fluid rounded"
+                                  style="height: 300px; width: 200px; object-fit: cover;  border: 5px solid #d7d7d7;"
+                                  alt="{{ $about->foto_kepala_dinas }}">
+                              </div>
                             </div>
-                          </div>
-                          <div class="row">
-                            <div class="col">
-                              <h6 class="text-center mb-0 mt-2"><strong>{{ $about->kepala_dinas }}</strong></h6>
-                              <div class="text-center">(Kepala Dinas)</div>
+                            <div class="row">
+                              <div class="col">
+                                <h6 class="text-center mb-0 mt-2"><strong>{{ $about->kepala_dinas }}</strong></h6>
+                                <div class="text-center">(Kepala Dinas)</div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
+                      <div class="col">
+                        <h5>Tentang Dinas Pendidikan</h5>
+                        {{ $about->tentang_disdik }}
+                        <hr>
+                        <h5>Quote</h5>
+                        <p>{{ $about->quotes }} - {{ $about->kepala_dinas }}</p>
+                      </div>
                     </div>
-                    <div class="col">
-                      <h5>Tentang Dinas Pendidikan</h5>
-                      {{ $about->tentang_disdik }}
-                      <hr>
-                      <h5>Quote</h5>
-                      <p>{{ $about->quotes }} - {{ $about->kepala_dinas }}</p>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <th class="col-2">Nomor Telepon</th>
-                <td>{{ $about->no_telepon }}</td>
-              </tr>
-              <tr>
-                <th>Email</th>
-                <td>{{ $about->email }}</td>
-              </tr>
-              <tr>
-                <th>Instagram</th>
-                <td>{{ $about->instagram }}</td>
-              </tr>
-              <tr>
-                <th>Alamat</th>
-                <td>{{ $about->alamat }}</td>
-              </tr>
-            
-            </table>
-            <a href="{{ route('about.update', ['id' => $about->id]) }}" class="btn btn-primary">Edit Data</a>
+                  </td>
+                </tr>
+                <tr>
+                  <th class="col-2">Nomor Telepon</th>
+                  <td>{{ $about->no_telepon }}</td>
+                </tr>
+                <tr>
+                  <th>Email</th>
+                  <td>{{ $about->email }}</td>
+                </tr>
+                <tr>
+                  <th>Instagram</th>
+                  <td>{{ $about->instagram }}</td>
+                </tr>
+                <tr>
+                  <th>Alamat</th>
+                  <td>{{ $about->alamat }}</td>
+                </tr>
+              </table>
+              <a href="{{ route('about.update', ['id' => $about->id]) }}" class="btn btn-primary">Edit Data</a>
+            @else
+              <p>Tidak ada informasi tersedia</p>
+              <a href="{{ route('about.create') }}" class="btn btn-primary">Create</a>
+            @endif
           </div>
         </div>
       </div>
